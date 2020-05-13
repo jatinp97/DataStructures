94858c86981226345bbab8a9ca2e078754dc2f70
jatinp97 <65271264+jatinp97@users.noreply.github.com>
2020-05-13 12:14:41 +0530

    Update BasicLinkedList.java

--- BasicLinkedList.java
+++ BasicLinkedList.java
@@ -120,14 +120,14 @@ public class BasicLinkedList {
 		assert(false);
 		return -1;
 	}
-	public Node middleNode(){
+	public int middleNode(){
 		Node slow=head;
 		Node fast=head;
 		while(fast!=null && fast.next!=null){
 			slow=slow.next;
 			fast=fast.next.next;
 		}
-		return slow;
+		return slow.data;
 	}
 	
 	
